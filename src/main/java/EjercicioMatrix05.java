a5da24d75d8d8c8f649d83eb7d96fc55443bc4ff
ArteMaxL <artemiolucero@gmail.com>
2022-03-12 19:32:21 -0300

    Ejercicio 5 completado

--- src/main/java/EjercicioMatrix05.java
+++ src/main/java/EjercicioMatrix05.java
@@ -1,3 +1,7 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EjercicioMatrix05 {
     public static void main(String[] args) {
         /*
@@ -12,5 +16,31 @@ public class EjercicioMatrix05 {
          de 15 posiciones en donde se deberá calcular y almacenar la cantidad de horas extras de cada obrero. En caso
          de que no haya realizado horas extras, deberá incluirse un número 0. Tomar como ejemplo la siguiente imagen:
         */
+        Scanner input = new Scanner(System.in);
+        int[][] horasTotal = new int[15][6];
+        int[] horasExtras = new int[horasTotal.length];
+        String[] apellidos = new String[horasTotal.length];
+
+        System.out.println("Carga las horas del personal de manera mensual");
+        System.out.println("El excedente de 160 horas por mes se considera horas extras");
+        System.out.println("************************************************************");
+
+        for (int i = 0; i < horasTotal.length; i++) {
+            System.out.println("Ingresa el apellido del empleado: ");
+            apellidos[i] = input.next();
+            for (int j = 0; j < 6; j++) {
+                System.out.print("Carga las horas del mes " + (j + 1) + ": ");
+                int carga = input.nextInt();
+                horasTotal[i][j] = carga;
+                if (carga > 160) {
+                    horasExtras[i] += carga - 160;
+                }
+            }
+            System.out.println("-------------------------------------");
+        }
+        for (int i = 0; i < horasExtras.length; i++) {
+            System.out.println("Empleado: " + apellidos[i] + " - Horas extras: " + horasExtras[i]);
+            System.out.println("-------------------------------------------");
+        }
     }
 }
